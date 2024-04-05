2fe95d32a60c891ea05e161fab5a42bbe5c4ca8b
Noah Rosamilia <noah@ivoah.net>
2024-04-04 23:24:33 -0400

    Don't use affine transforms

--- Source/main.lua
+++ Source/main.lua
@@ -1,52 +1,68 @@
-gfx = playdate.graphics
-
-import 'CoreLibs/object'
-
-local hourHand <const> = playdate.geometry.lineSegment.new(0, 0, 0, 50)
-local minuteHand <const> = playdate.geometry.lineSegment.new(0, 0, 0, 100)
-local secondHand <const> = playdate.geometry.lineSegment.new(0, 0, 0, 100)
+local gfx = playdate.graphics
 
 local clockColor = gfx.kColorWhite
-
-local checkmarkMenuItem, error = playdate.getSystemMenu():addCheckmarkMenuItem("XOR", false, function(checked)
+playdate.getSystemMenu():addCheckmarkMenuItem("XOR", false, function(checked)
     clockColor = checked and gfx.kColorXOR or gfx.kColorWhite
 end)
+
+local thickness = false
+playdate.getSystemMenu():addCheckmarkMenuItem("thickness", false, function(checked)
+    thickness = checked
+end)
+
+local ms = 0
+local depth = 5
+local shrinkage = 1
+
+gfx.setLineCapStyle(playdate.graphics.kLineCapStyleRound)
 
 function playdate:update()
 	gfx.setColor(gfx.kColorBlack)
 	gfx.fillRect(0, 0, 400, 240)
 	gfx.setColor(clockColor)
 
-	playdate.drawFPS(0, 0)
-
 	local time = playdate.getTime()
+	if (playdate.isCrankDocked()) then
+		ms += (time.hour*60*60*1000 + time.minute*60*1000 + time.second*1000 + time.millisecond - ms)/2
+	else
+		ms += playdate.getCrankChange()*100
+	end
 
-	local ms = time.hour*60*60*1000 + time.minute*60*1000 + time.second*1000 + time.millisecond
+	if (playdate.buttonJustPressed(playdate.kButtonUp)) then
+		depth += 1
+	elseif (playdate.buttonJustPressed(playdate.kButtonDown)) then
+		depth -= 1
+	elseif (playdate.buttonJustPressed(playdate.kButtonLeft)) then
+		shrinkage -= 0.1
+	elseif (playdate.buttonJustPressed(playdate.kButtonRight)) then
+		shrinkage += 0.1
+	end
 
-	drawClock(ms, playdate.geometry.point.new(200, 120), 180, 3)
+	drawClock(ms, 200, 120, -math.pi/2, 100, depth, true)
 
 end
 
-function drawClock(ms, center, angle, depth, noHour)
+function drawClock(ms, x, y, angle, length, depth, drawHour)
 	if depth < 0 then return end
 
-	local centerAffine <const> = playdate.geometry.affineTransform.new()
-	centerAffine:rotate(angle)
-	centerAffine:translate(center.x, center.y)
+	local hourAngle = angle + ms/1000/60/60/12 * 2*math.pi
+	local minuteAngle = angle + ms/1000/60/60 * 2*math.pi
+	local secondAngle = angle + ms/1000/60 * 2*math.pi
 
-	local hourAffine = centerAffine:copy()
-	hourAffine:rotate(ms/1000/60/60/12 * 360, center.x, center.y)
-	local minuteAffine = centerAffine:copy()
-	minuteAffine:rotate(ms/1000/60/60 * 360, center.x, center.y)
-	local secondAffine = centerAffine:copy()
-	secondAffine:rotate(ms/1000/60 * 360, center.x, center.y)
+	local hx, hy = x + math.cos(hourAngle)*length/2, y + math.sin(hourAngle)*length/2
+	local mx, my = x + math.cos(minuteAngle)*length, y + math.sin(minuteAngle)*length
+	local sx, sy = x + math.cos(secondAngle)*length, y + math.sin(secondAngle)*length
 
-	if not noHour then
-		gfx.drawLine(hourAffine:transformedLineSegment(hourHand))
+	if thickness then
+		gfx.setLineWidth(depth + 1)
 	end
-	gfx.drawLine(minuteAffine:transformedLineSegment(minuteHand))
-	gfx.drawLine(secondAffine:transformedLineSegment(secondHand))
 
-	drawClock(ms, minuteAffine:transformedPoint(playdate.geometry.point.new(0, 100)), angle + (ms/1000/60/60 * 360) - (ms/1000/60/60/12 * 360) + 180, depth - 1, true)
-	drawClock(ms, secondAffine:transformedPoint(playdate.geometry.point.new(0, 100)), angle + (ms/1000/60 * 360) - (ms/1000/60/60/12 * 360) + 180, depth - 1, true)
+	if drawHour then
+		gfx.drawLine(x, y, hx, hy)
+	end
+	gfx.drawLine(x, y, mx, my)
+	gfx.drawLine(x, y, sx, sy)
+
+	drawClock(ms, mx, my, minuteAngle - (hourAngle - angle) - math.pi, length*shrinkage, depth - 1)
+	drawClock(ms, sx, sy, secondAngle - (hourAngle - angle) - math.pi, length*shrinkage, depth - 1)
 end
